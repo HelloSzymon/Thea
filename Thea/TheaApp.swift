9e21d7f4560edd6b24fc8ab12cced7c8e96b2c34
Szymon Wnuk <szymon@bereyziat.dev>
2026-03-04 23:20:58 +0100

    Initial Commit

---
+++ Thea/TheaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheaApp.swift
+//  Thea
+//
+//  Created by Szymon Wnuk on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TheaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheaTests/TheaTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheaTests.swift
+//  TheaTests
+//
+//  Created by Szymon Wnuk on 04/03/2026.
+//
+
+import Testing
+@testable import Thea
+
+struct TheaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
