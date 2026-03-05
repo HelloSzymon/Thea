52a7aa4ec02ceef929e36b9a5a50ba37d8970a7b
Szymon Wnuk <szymon@bereyziat.dev>
2026-03-05 21:59:17 +0100

    feat: picker

--- Thea/SettingsView.swift
+++ Thea/SettingsView.swift
@@ -7,9 +7,16 @@
 
 import SwiftUI
 
+enum VolumeUnit: String, CaseIterable {
+    case liters
+    case gallons
+}
+
 struct SettingsView: View {
     @AppStorage("defaultFermentationDays")
     private var defaultFermentationDays: Int = 7
+    @AppStorage("volumeUnit")
+     private var volumeUnit: String = VolumeUnit.liters.rawValue
     var body: some View {
         NavigationStack {
             Form{
@@ -17,6 +24,12 @@ struct SettingsView: View {
                     Stepper("Default days \(defaultFermentationDays)", value: $defaultFermentationDays, in: 1...30)
                         .padding()
                 }
+
+                Picker("Volume unit", selection: $volumeUnit, content: {
+                    ForEach(VolumeUnit.allCases, id: \.self) {
+                        Text($0.rawValue.capitalized).tag($0.rawValue)
+                    }
+                }) .padding()
             }
 
 
