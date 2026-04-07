2fc2308a4af0de8a5c2a363964a2ee04a02db10f
Szymon Wnuk <szymon@bereyziat.dev>
2026-04-07 23:55:56 +0200

    fix: refactor

---
+++ Thea/Components/EmptyStateView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyStateView.swift
+//  Thea
+//
+//  Created by Szymon Wnuk on 07/04/2026.
+//
+
+import SwiftUI
+
+struct EmptyStateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyStateView()
+}
