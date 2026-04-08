fe452fb39a515546852120ae29cba0694f4def73
Szymon Wnuk <szymon@bereyziat.dev>
2026-04-08 16:36:53 +0200

    feat: progress bar

--- Thea/View/BatchDetailView.swift
+++ Thea/View/BatchDetailView.swift
@@ -11,12 +11,22 @@ struct BatchDetailView: View {
 
     var batch: Batch
     var body: some View {
-        VStack{
+        VStack(spacing: 16){
             Text(batch.name)
+                .font(.title)
+                    .bold()
             Text(batch.status.displayName)
+                .foregroundStyle(batch.status.batchColor)
+            Divider()
             Text("\(batch.volume) \(batch.unit.rawValue)")
-            Text("\(batch.daysFermenting)")
             Text(batch.startDate.formatted())
+            Text("\(batch.daysFermenting)")
+            Divider()
+
+
+            ProgressView(value: 1, total: 14)
+                .progressViewStyle(.linear)
+                .padding()
         }}
 }
 
